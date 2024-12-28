80c55bfa3ef75160f25bf5b5d25caefed5502722
TOAH-X <124673039+TOAH-X@users.noreply.github.com>
2024-12-28 20:34:16 +0900

    チーム変更

--- Assets/Script/UIController/ChangeTeam/ChangeTeamTeamMemberController.cs
+++ Assets/Script/UIController/ChangeTeam/ChangeTeamTeamMemberController.cs
@@ -6,9 +6,17 @@ public class ChangeTeamTeamMemberController : MonoBehaviour
 {
     [SerializeField] TeamCoutnroller teamCoutnrollerScript;         //teamCoutnrollerのスクリプト
 
+    [SerializeField] GameObject TeamChar1;                          //チームキャラ1
+    [SerializeField] GameObject TeamChar2;                          //チームキャラ2
+    [SerializeField] GameObject TeamChar3;                          //チームキャラ3
+
+    private int[] teamIdData = new int[3];                          //チームデータ(ID)
+
     // Start is called before the first frame update
     void Start()
     {
+        teamIdData = teamCoutnrollerScript.TeamIdData;
+
         
     }
 
