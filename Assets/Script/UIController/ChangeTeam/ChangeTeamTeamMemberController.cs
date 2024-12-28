90895dad4041a3305bf9e6fa8b86d00b82678c75
TOAH-X <124673039+TOAH-X@users.noreply.github.com>
2024-12-28 21:22:10 +0900

    仮画像追加

--- Assets/Script/UIController/ChangeTeam/ChangeTeamTeamMemberController.cs
+++ Assets/Script/UIController/ChangeTeam/ChangeTeamTeamMemberController.cs
@@ -1,28 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeTeamTeamMemberController : MonoBehaviour
 {
     [SerializeField] TeamCoutnroller teamCoutnrollerScript;         //teamCoutnrollerのスクリプト
 
-    [SerializeField] GameObject TeamChar1;                          //チームキャラ1
-    [SerializeField] GameObject TeamChar2;                          //チームキャラ2
-    [SerializeField] GameObject TeamChar3;                          //チームキャラ3
+    [SerializeField] GameObject teamChar1Obj;                       //チームキャラ1(バックグラウンド)
+    [SerializeField] GameObject teamChar2Obj;                       //チームキャラ2(バックグラウンド)
+    [SerializeField] GameObject teamChar3Obj;                       //チームキャラ3(バックグラウンド)
 
     private int[] teamIdData = new int[3];                          //チームデータ(ID)
 
     // Start is called before the first frame update
     void Start()
     {
-        teamIdData = teamCoutnrollerScript.TeamIdData;
-
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        teamIdData = teamCoutnrollerScript.TeamIdData;
+
+        //ChangeTeamControllerオブジェクトの取得
+        GameObject uiCanvasObj = transform.parent.parent.gameObject;
+        GameObject changeTeamControllerObj = uiCanvasObj.transform.Find("ChangeTeamController").gameObject;
+        ChangeTeamController changeTeamControllerObjScript = changeTeamControllerObj.GetComponent<ChangeTeamController>();
+
+
+        GameObject teamChar1IconObj = teamChar1Obj.transform.Find("TeamChar1Icon").gameObject;
+        Image teamChar1IconObjImage = teamChar1IconObj.GetComponent<Image>();
+        teamChar1IconObjImage.sprite = changeTeamControllerObjScript.CharDbReferenceCharFullBodyImage(teamIdData[0]);
+
+        GameObject teamChar2IconObj = teamChar2Obj.transform.Find("TeamChar2Icon").gameObject;
+        Image teamChar2IconObjImage = teamChar2IconObj.GetComponent<Image>();
+        teamChar2IconObjImage.sprite = changeTeamControllerObjScript.CharDbReferenceCharFullBodyImage(teamIdData[1]);
+
+        GameObject teamChar3IconObj = teamChar3Obj.transform.Find("TeamChar3Icon").gameObject;
+        Image teamChar3IconObjImage = teamChar3IconObj.GetComponent<Image>();
+        teamChar3IconObjImage.sprite = changeTeamControllerObjScript.CharDbReferenceCharFullBodyImage(teamIdData[2]);
+    }
+
+    //アイコンのリフレッシュ。チームメンバーを変えたとき、もしくは開いた瞬間のみ呼び出す
+    public void ChangeTeamMember() 
+    { 
     }
 }
